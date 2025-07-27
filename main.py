a248f632d4edeb6d4449c75789e3f6d44e50079a
jeanluc <j.mucyonda@alustudent.com>
2025-07-27 14:53:37 +0200

    display menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from cli import print_header
+from cli import print_header, print_menu
 from db_handler import initialize_db
 
 
@@ -7,8 +7,25 @@ def main():
     initialize_db()
 
     print_header()
-    pass
+    while True:
+        print_menu()
+        choice = input("Choose (1–6): ")
 
-
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        elif choice == '5':
+            pass
+        elif choice == '6':
+            print("\nExiting. Stay safe!\n")
+            break
+        else:
+            print("\nInvalid choice. Please enter a number between 1 and 6.")
+            
 if __name__ == "__main__":
     main()
